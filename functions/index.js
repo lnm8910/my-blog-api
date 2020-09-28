f0fb49b4a3e0104b8a95612ee99cbc49c6e1a9f5
Lalit Narayan Mishra <lnm8910@gmail.com>
2020-09-27 23:50:12 -0400

    Added other CRUD operations.

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -27,5 +27,76 @@ app.post('/api/create', (req, res) => {
         }
     })();
 });
+
+// read item
+app.get('/api/read/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.params.item_id);
+            let item = await document.get();
+            let response = item.data();
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+// read all
+app.get('/api/read', (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection('items');
+            let response = [];
+            await query.get().then(querySnapshot => {
+                let docs = querySnapshot.docs;
+                for (let doc of docs) {
+                    const selectedItem = {
+                        id: doc.id,
+                        item: doc.data().item
+                    };
+                    response.push(selectedItem);
+                }
+                return null;
+            });
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+// update
+app.put('/api/update/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.params.item_id);
+            await document.update({
+                item: req.body.item
+            });
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+// delete
+app.delete('/api/delete/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.params.item_id);
+            await document.delete();
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
 
 exports.app = functions.https.onRequest(app);
