b0b810e634a777dbc6975ff1dfc0d35fcdbf4a47
Lalit Narayan Mishra <lnm8910@gmail.com>
2020-09-27 22:28:22 -0400

    First commit

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const serviceAccount = require("./permissions.json");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true }));
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://fir-api-9a206..firebaseio.com"
+});
+const db = admin.firestore();
+
+app.get('/hello-world', (req, res) => {
+    return res.status(200).send('Hello World!');
+});
+
+app.post('/api/create', (req, res) => {
+    (async () => {
+        try {
+            await db.collection('items').doc('/' + req.body.id + '/')
+                .create({item: req.body.item});
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+exports.app = functions.https.onRequest(app);
